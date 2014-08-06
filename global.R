a563a8559ebf842fab199278d268c381cba688dd
Fatore <fmfatore@gmail.com>
2014-08-06 18:18:11 -0300

    added a install dependecies to global.R (not the best practice)

--- global.R
+++ global.R
@@ -1,4 +1,9 @@
 # Any code in this file is guaranteed to be called before either
 # ui.R or server.R
 
+# TODO: remove this check from here and add to Depends in package declaration
+list.of.packages <- c("plotrix", "tsne", "mp")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 source("charts.R")
